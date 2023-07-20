f707acb4c4eb380c13d4baf8e4f843c4d765042d
akshithananthula <akshith6212@gmail.com>
2023-07-20 10:50:15 +0530

    init commit

---
+++ src/test/java/in/cms/tool/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.cms.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
